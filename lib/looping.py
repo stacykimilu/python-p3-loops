9502ce189fba7487e9ca6f9d7a005fe67ba21d70
Stacy <stacykimilu@gmail.com>
2024-05-23 09:42:16 +0300

    python loops completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+  return [num ** 2 for num in int_list]
+print(square_integers([1, 2, 3, 4]))  
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101): 
+        print(fizzbuzz_helper(num))
+    
+def fizzbuzz_helper(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+
+fizzbuzz()
+
+  
